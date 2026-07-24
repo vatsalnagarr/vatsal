0096f291a88b8f33e07988cbae5438617879047d
vatsal <nagarvatsal196@gmail.com>
2026-07-24 23:28:03 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (48.38%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int searchInsert(int[] nums, int x) {
+            int n = nums.length;
+        int low = 0, high = n - 1;
+        int ans = n; // Default to end if x is greater than all elements
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] >= x) {
+                // Potential answer found, try to go left
+                ans = mid;
+                high = mid - 1;
+            } else {
+                // Go right
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+}
